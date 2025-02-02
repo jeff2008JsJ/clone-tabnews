177ddbfdfb00f7e85a08d5c82ea395b16bc760d5
jeff2008JsJ <jeff2008jl@gmail.com>
2025-02-02 20:14:27 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para programação https://www.tabnews para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1> Samuka e ai blz, weekend you go to house?</h1>
+  return <h1> Samuka e ai blz, weekend you go to house?</h1>;
+}
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
